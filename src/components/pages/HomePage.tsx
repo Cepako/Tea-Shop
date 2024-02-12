5e9b6eb4d133c2c97ea3af644c41668334f06456
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-02-12 22:59:34 +0100

    create info component

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Welcome from './home-page-components/Welcome';
 import Arrivals from './home-page-components/Arrivals';
 import ReadOurBlog from './home-page-components/ReadOurBlog';
+import Info from './home-page-components/Info';
 
 import './HomePage.scss';
 
@@ -12,6 +13,7 @@ const Page: React.FC = () => {
       <Welcome />
       <Arrivals />
       <ReadOurBlog />
+      <Info />
     </div>
   );
 };
