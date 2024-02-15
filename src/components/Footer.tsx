271288ce268f2c8689e3eeab2d05c6f3d39b7279
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-02-15 18:46:16 +0100

    add media queries for footer and GoUpButton components and add different arrow-up colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer: React.FC = () => {
-  return <div className="footer"> &copy;2024 Sebastian Dragan</div>;
+  const scrollUpHandler = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="footer">
+      &copy;2024 Sebastian Dragan
+      <span onClick={scrollUpHandler}>
+        <img src="./images/arrow-up-black.svg" alt="arrow up" /> Back to top
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
