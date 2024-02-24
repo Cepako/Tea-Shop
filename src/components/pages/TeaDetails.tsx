be898e606cd33ff2c0b39bc8524c50a1256e43bd
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-02-24 18:54:02 +0100

    create and style TeaProductInfo component

--- src/components/pages/TeaDetails.tsx
+++ src/components/pages/TeaDetails.tsx
@@ -3,25 +3,27 @@ import { useParams } from 'react-router-dom';
 import teasData from './tea-details-components/tea-details-data';
 import Header from './tea-details-components/Header';
 import TeaProduct from './tea-details-components/TeaProduct';
+import TeaProductInfo from './tea-details-components/TeaProductInfo';
 
 import './TeaDetails.scss';
 
 const TeaDetails: React.FC = () => {
   const { productLink } = useParams<string>();
   const teaData = teasData.filter((tea) => tea.link === productLink);
-  const { name, price, product_img, product_description, product_info } =
+  const { name, price, code, product_img, product_description, product_info } =
     teaData[0];
 
   return (
     <div className="tea-details">
       <Header />
       <TeaProduct
-        product_img={product_img}
         name={name}
         price={price}
+        code={code}
+        product_img={product_img}
         product_description={product_description}
-        product_info={product_info}
       />
+      <TeaProductInfo product_info={product_info} />
     </div>
   );
 };
---
+++ src/components/pages/tea-details-components/TeaProductInfo.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './TeaProductInfo.scss';
+
+interface TPIInterface {
+  product_info: string;
+}
+
+const TeaProductInfo: React.FC<TPIInterface> = ({ product_info }) => {
+  const [infoActive, setInfoActive] = useState(true);
+  const [policyActive, setPolicyActive] = useState(false);
+  const [shippingActive, setShippingActive] = useState(false);
+
+  const handleToggle = (section: string) => {
+    switch (section) {
+      case 'info':
+        setShippingActive(false);
+        setPolicyActive(false);
+        setInfoActive(!infoActive);
+        break;
+      case 'policy':
+        setInfoActive(false);
+        setShippingActive(false);
+        setPolicyActive(!policyActive);
+        break;
+      case 'shipping':
+        setInfoActive(false);
+        setPolicyActive(false);
+        setShippingActive(!shippingActive);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="product-info-wrapper">
+      <div
+        id="infoDiv"
+        className={`product-info ${infoActive ? 'active' : ''}`}
+      >
+        <h3 onClick={() => handleToggle('info')}>
+          Product Info <span>{infoActive ? '-' : '+'}</span>
+        </h3>
+        <p>{product_info}</p>
+      </div>
+      <div
+        id="policyDiv"
+        className={`product-info ${policyActive ? 'active' : ''}`}
+      >
+        <h3 onClick={() => handleToggle('policy')}>
+          Return & Refund Policy <span>{policyActive ? '-' : '+'}</span>
+        </h3>
+        <p>
+          If you are unhappy with your purchase, you have the right to return
+          the product within a specified period from the date of purchase. If
+          you receive a faulty or damaged product, please promptly inform us of
+          the situation. We are ready to take corrective actions, exchange the
+          product, or offer a refund, depending on your preferences.
+        </p>
+      </div>
+      <div
+        id="shippingDiv"
+        className={`product-info ${shippingActive ? 'active' : ''}`}
+      >
+        <h3 onClick={() => handleToggle('shipping')}>
+          Shipping Info <span>{shippingActive ? '-' : '+'}</span>
+        </h3>
+        <p>
+          Your order is processed within 1-2 business days. Shipping costs
+          depend on the delivery location. You will receive a tracking number,
+          and available options include standard or express delivery. Ensure to
+          provide an accurate delivery address to avoid delays. Thank you for
+          shopping with us!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TeaProductInfo;
