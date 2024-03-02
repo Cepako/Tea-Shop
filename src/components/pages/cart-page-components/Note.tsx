eecfd461c84d08d1a4765b340db385910ffd6ab6
Cepako <sebastian.dragan305@gmail.com>
2024-03-02 23:00:57 +0100

    add function that focus on input on paragraph click and change styles

--- src/components/pages/cart-page-components/Note.tsx
+++ src/components/pages/cart-page-components/Note.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './Note.scss';
 
 const Note: React.FC = () => {
   const [viewInput, setViewInput] = useState(false);
+
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleClick = () => {
+    setViewInput((prev) => !prev);
+    if (!viewInput)
+      setTimeout(() => {
+        inputRef.current!.focus();
+      }, 100);
+  };
+
   const input = (
-    <textarea placeholder='Instructions? Special requests? Add them here.' />
+    <textarea
+      placeholder='Instructions? Special requests? Add them here.'
+      ref={inputRef}
+    />
   );
 
   return (
     <div className='note'>
-      <p onClick={() => setViewInput((prev) => !prev)}>
+      <p onClick={handleClick}>
         <img src='./images/notes-icon.svg' alt='notes' />
         Add a note
       </p>
--- src/components/pages/cart-page-components/PromoCode.tsx
+++ src/components/pages/cart-page-components/PromoCode.tsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import './PromoCode.scss';
 
 const PromoCode: React.FC = () => {
   const [viewInput, setViewInput] = useState(false);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    setViewInput((prev) => !prev);
+    if (!viewInput)
+      setTimeout(() => {
+        inputRef.current!.focus();
+      }, 100);
+  };
+
   const input = (
     <>
-      <input type='text' placeholder='Enter a promo code' />
+      <input type='text' placeholder='Enter a promo code' ref={inputRef} />
       <button>Apply</button>
     </>
   );
 
   return (
     <div className='promo-code'>
-      <p
-        onClick={() => {
-          setViewInput((prev) => !prev);
-        }}
-      >
+      <p onClick={handleClick}>
         <img src='./images/price-tag-icon.svg' alt='price tag' />
         Enter a promo code
       </p>
