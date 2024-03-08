e805a60ca4121d359d8b42e9bcbb31d935a93712
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-03-08 17:53:54 +0100

    add initial values to filters when open teas page

--- src/components/pages/Teas.tsx
+++ src/components/pages/Teas.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './tea-details-components/Header';
 import Filters from './teas-page-components/Filters';
 import TeaProducts from './teas-page-components/TeaProducts';
+import { useAppDispatch } from '../../redux/hooks';
+import { editTeasFilters } from '../../redux/filters';
 
 import './Teas.scss';
 
 const Teas: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const initialPayload: {
+      collection: string;
+      price: [number, number];
+      size: string[];
+    } = {
+      collection: 'all',
+      price: [5, 15],
+      size: [],
+    };
+    dispatch(editTeasFilters(initialPayload));
+  }, []);
   return (
     <div className='teas-page'>
       <Header />
