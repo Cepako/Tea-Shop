c50de4b88179b66c5bcf4d8d1dc9cde7294a243d
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-03-17 17:07:01 +0100

    add extras to product data

--- src/data.ts
+++ src/data.ts
@@ -125,5 +125,89 @@ const data = [
     product_info:
       'Mint & Melissa Mix is a refreshing tea, combining the freshness of mint with the gentleness of melissa in a harmonious blend of black tea. The intense aroma of mint and melissa gives the beverage a unique character, creating an unforgettable taste experience.',
   },
+  {
+    collection: 'extras',
+    link: 'classic-tea-pot',
+    name: 'Classic Tea Pot',
+    price: '20.00',
+    code: 'SKU: 0011',
+    color: ['Black', 'Violet'],
+    product_img: '/images/extras/teapot1.png',
+    hover_img: '/images/extras/teapot1.1.png',
+    product_description:
+      'The Classic Tea Pot is an essential element of any elegant kitchen. Crafted from high-quality porcelain, our tea pot ensures durability and a refined look. Its classic design is enriched with delicate decorations, adding a touch of nobility. With a capacity tailored to your needs, our tea pot is perfect for gatherings with friends or family.   ',
+    product_info:
+      'Material: high-quality porcelain. Capacity: available in various sizes to suit your needs. Classic design with delicate decorations. Durable and easy to clean. Perfect gift for tea enthusiasts and lovers of elegant kitchen accessories. Enjoy aromatic tea served with elegance and style thanks to our classic tea pot!',
+  },
+  {
+    collection: 'extras',
+    link: 'small-tea-pot',
+    name: 'Small Tea Pot',
+    price: '17.00',
+    code: 'SKU: 0001',
+    color: ['Brown', 'Violet'],
+    product_img: '/images/extras/teapot2.png',
+    hover_img: '/images/extras/teapot2.2.png',
+    product_description:
+      'The Small Tea Pot is a charming addition to any kitchen. Made from high-quality porcelain, this petite tea pot exudes elegance and functionality. Its compact size makes it perfect for individual servings or intimate gatherings. With its classic design and durable construction, the Small Tea Pot is both practical and stylish.         ',
+    product_info:
+      'Material: high-quality porcelain. Compact size ideal for individual servings. Classic design for timeless appeal. Durable construction for long-lasting use. Perfect for personal use or as a thoughtful gift for tea lovers. Enjoy a delightful cup of tea with this beautifully crafted accessory.',
+  },
+  {
+    collection: 'extras',
+    link: 'vintage-tea-pot',
+    name: 'Vintage Tea Pot',
+    price: '19.00',
+    code: 'SKU: 0015',
+    color: ['Blue-Grey', 'Violet'],
+    product_img: '/images/extras/teapot3.png',
+    hover_img: '/images/extras/teapot3.3.png',
+    product_description:
+      'The Vintage Tea Pot adds a touch of nostalgia and elegance to your tea time experience. Crafted with exquisite attention to detail, this tea pot showcases a timeless vintage design that evokes a sense of charm and sophistication. Made from high-quality materials, it boasts durability and functionality.  ',
+    product_info:
+      "Material: premium-quality materials. Vintage design for a charming aesthetic. Sturdy construction ensures longevity. Perfect for special occasions or everyday use. Whether you're hosting a tea party or enjoying a quiet moment alone, the Vintage Tea Pot promises to elevate your tea-drinking ritual with its classic allure.",
+  },
+  {
+    collection: 'extras',
+    link: 'cup',
+    name: 'Cup',
+    price: '5.00',
+    code: 'SKU: 0014',
+    color: ['Grey', 'Brown'],
+    product_img: '/images/extras/cup1.png',
+    hover_img: '/images/extras/cup1.1.png',
+    product_description:
+      'The Cup is a versatile and essential vessel for enjoying your favorite beverages. Crafted with care from durable materials, our cups offer both style and functionality. With a sleek and timeless design, they seamlessly complement any table setting or decor theme. ',
+    product_info:
+      "Material: high-quality ceramic or porcelain. Classic design suitable for any occasion. Durable construction for long-lasting use. Whether you're sipping morning coffee or indulging in an afternoon tea, our cups are designed to enhance your drinking experience with their elegant simplicity.",
+  },
+  {
+    collection: 'extras',
+    link: 'tea-ball-infuser',
+    name: 'Tea Ball Infuser',
+    price: '2.00',
+    code: 'SKU: 0012',
+    color: [],
+    product_img: '/images/extras/tea-ball-infuser.png',
+    hover_img: '/images/extras/tea-ball-infuser.png',
+    product_description:
+      'The Tea Ball Infuser is the perfect tool for brewing loose leaf tea with ease and convenience. Crafted from high-quality stainless steel, this infuser features a fine mesh design that allows for optimal steeping and flavor extraction. Its compact size and easy-to-use mechanism make it ideal for individual servings or large pots of tea.  ',
+    product_info:
+      'Material: premium-quality stainless steel. Fine mesh design ensures thorough steeping and flavor release. Compact size for individual servings or large pots of tea. Easy-to-use mechanism for hassle-free brewing. Simply fill the infuser with your favorite tea leaves, steep in hot water, and enjoy a perfectly brewed cup every time.',
+  },
+  {
+    collection: 'extras',
+    link: 'set-of-cups',
+    name: 'Set Of Cups',
+    price: '15.00',
+    code: 'SKU: 0013',
+    color: ['Grey', 'Light-Green'],
+    product_img: '/images/extras/set-of-cups.png',
+    hover_img: '/images/extras/set-of-cups1.png',
+    product_description:
+      'The set of cups is an essential addition to your kitchen. Our cups are made of high-quality ceramic, ensuring durability and elegance. In the set, you will find five cups of various capacities, perfect for everyday use and special occasions. Their universal design makes them suitable for any interior decor, adding a touch of sophistication. ',
+    product_info:
+      'The material they are made of is ceramics. Set includes 3 cups of various capacities. Durable and easy to clean. Universal design suitable for any interior decor. Perfect gift for tea, coffee, and other beverage enthusiasts.',
+  },
 ];
 export default data;
