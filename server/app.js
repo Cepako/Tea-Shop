b6907e67385b9ac473f69625376d3f8dda31308f
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-04-17 21:32:24 +0200

    connect server and database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const adminRoutes = require('./routes/admin');
 
 const app = express();
 
-
 app.use(bodyParser.json());
 
-app.listen(8080);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/admin', adminRoutes);
+
+mongoose
+  .connect(
+    'mongodb+srv://Sebastian:<PASSWORD>@tea-shop.le4eiik.mongodb.net/tea-shop?retryWrites=true&w=majority&appName=Tea-Shop'
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
