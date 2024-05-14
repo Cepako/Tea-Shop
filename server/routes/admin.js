3deff2bc69b61ec0ebde15952baa841b6197cc92
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-05-14 18:24:32 +0200

    delete useless validation from server side

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -8,23 +8,6 @@ const router = express.Router();
 const validateProductInputs = [
   body('type').isIn(['tea', 'extras']),
   body('name').trim().isLength({ min: 5 }),
-  body('group').trim().isLength({ min: 5 }),
-  body('size').custom((value, { req }) => {
-    if (!req.body.color) {
-      if (!value || value.trim().length < 3) {
-        throw new Error('Size field is incorrect.');
-      }
-    }
-    return true;
-  }),
-  body('color').custom((value, { req }) => {
-    if (!req.body.size) {
-      if (!value || value.length !== 2) {
-        throw new Error('Color field is incorrect.');
-      }
-    }
-    return true;
-  }),
   body('description').trim().isLength({ min: 15 }),
   body('info').trim().isLength({ min: 15 }),
 ];
